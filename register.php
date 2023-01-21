7a08d8bda965c0192c3af96aa9f0a33c444aff9a
Danilo da Silva <a00264112@student.ait.ie>
2023-01-21 16:14:40 +0000

    Register Page html and css

---
+++ register.php
@@ -0,0 +1,45 @@
+<?php
+    //Check if submit button is pressed
+    if(isset($_POST['submitButton'])){
+
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome to Movies</title>
+    <link rel="stylesheet" href="assets/style/style.css">
+</head>
+<body>
+    <div class="signInContainer">
+        <div class="imgContainer">
+            <img src="assets/images/login-background.jpg" class="login-background" alt="login-background.jpg">
+        </div>
+        <img src="assets/images/logo.png" class="logo" title="logo" alt="logo.png">
+        <div class="column">
+
+            <div class="header">
+                <h3>Sign Up</h3>
+                <span>to continue to Movies</span>
+            </div>
+
+            <form method="POST">
+                <input type="text" name="firstName" placeholder="First Name" required>
+                <input type="text" name="lastName" placeholder="Last Name" required>
+                <input type="text" name="username" placeholder="Username" required>
+                <input type="email" name="email" placeholder="Email" required>
+                <input type="email" name="email2" placeholder="Confirm Email" required>
+                <input type="password" name="password" placeholder="Password" required>
+                <input type="password" name="password2" placeholder="Confirm Password" required>
+                <input type="submit" name="submitPassword" value="Submit">
+            </form>
+
+            <a href="login.php" class="signInMessage">Already have an account? Sign in here!</a>
+        </div>
+    </div>
+</body>
+</html>
