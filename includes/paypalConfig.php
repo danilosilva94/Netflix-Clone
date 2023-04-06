7587c0222de60e7a4044c97ff5a6eda510bda48e
Danilo da Silva <102324724+danilosilva94@users.noreply.github.com>
2023-04-06 11:35:10 +0100

    Remove paypal credentials

--- includes/paypalConfig.php
+++ includes/paypalConfig.php
@@ -4,8 +4,8 @@
     //Api Context
     $apiContext = new \PayPal\Rest\ApiContext(
         new \PayPal\Auth\OAuthTokenCredential(
-            'AY_cE-kWy-1T6tONMRjvDpJqf80C-WQiRscrWJr8RrUDxjkE2kDE4pCrd1qGck2Spl4Ce72eashxsdCJ',     // ClientID
-            'EGoqg7i73RGRj4IXib6ofIDkGFp6w1u2b6Y9vOBfB6l64lNy6Ju9eAjcZpGnIuHGqtqGDOH1Q-yvwUdN'  // ClientSecret
+            '',     // ClientID
+            ''  // ClientSecret
         )
     );
 ?>
